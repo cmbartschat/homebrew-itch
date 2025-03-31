626ea836e4d59f8affa218bb788fa3e576585655
Christoph Bartschat <christoph@barts.chat>
2025-03-30 17:22:37 -0700

    v0.8.0

--- Formula/itch.rb
+++ Formula/itch.rb
@@ -1,7 +1,7 @@
 class Itch < Formula
   homepage "https://github.com/cmbartschat/itch"
-  url "https://github.com/cmbartschat/itch/archive/refs/tags/v0.7.0.zip"
-  sha256 "f7c59e5e3cc8afc6e8ffed223f43d7be077a605339683c33b3e8705c89ffec60"
+  url "https://github.com/cmbartschat/itch/archive/refs/tags/v0.8.0.zip"
+  sha256 "bc2d9ef6d5658932ca5e7692baf21b011d228499f1b22d210e37dc7444b630fa"
 
   depends_on "rust" => :build
 
@@ -11,6 +11,6 @@ class Itch < Formula
   end
 
   test do
-    assert_match "itch 0.7.0", shell_output("#{bin}/itch --version")
+    assert_match "itch 0.8.0", shell_output("#{bin}/itch --version")
   end
 end
